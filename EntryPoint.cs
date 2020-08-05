6c3695082c5c9e668c34965843cea618fb21110d
Martin Pereyra <pereyra.gamboa@outlook.com>
2020-08-05 18:48:49 -0500

    Add project files.

---
+++ EntryPoint.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="EntryPoint.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace QuantumDie
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.Quantum.Simulation.Core;
+    using Microsoft.Quantum.Simulation.Simulators;
+
+    /// <summary>
+    /// Entry point of the quantum host.
+    /// </summary>
+    public class EntryPoint
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        public static void Main()
+        {
+            int[] counts = new int[6];
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int i = 0; i < 600; i++)
+                {
+                    int result = (int)QuantumDie.Run(qsim).Result;
+                    counts[result]++;
+                }
+            }
+
+            int n = 0;
+            foreach (int count in counts)
+            {
+                Console.WriteLine("{0} = {1} / 600", ++n, count);
+            }
+        }
+    }
+}
---
+++ Program.qs
@@ -0,0 +1,27 @@
+﻿namespace QuantumDie {
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Math;
+    open Microsoft.Quantum.Measurement;
+    
+    @EntryPoint()
+    operation QuantumDie () : Int {
+        mutable res = 0;
+        let theta = 2. * ArcCos(Sqrt(2. / 3.));
+
+        using (qs = Qubit[3]) {
+            H(qs[0]);
+            Rx(theta, qs[2]);
+            X(qs[2]);
+            Controlled H([qs[2]], qs[1]);
+            X(qs[2]);
+
+            let ms = MultiM(qs);
+            if (ms[0] == One) { set res += 1; }
+            if (ms[1] == One) { set res += 2; }
+            if (ms[2] == One) { set res += 4; }
+        }
+
+        return res;
+    }
+}
